877ab7f1dd49f9888b19dc3ccfb8465ceda12b49
zorin <andrei.zorin@gropyus.com>
2025-05-02 17:14:31 +0200

    add title

--- src/views/layouts/index.tsx
+++ src/views/layouts/index.tsx
@@ -4,6 +4,7 @@ export default function Layout({children}: { children: JSX.Element }) {
     return (
         <html>
         <head>
+            <title>Demo Bun + Hono App</title>
             <script src="https://cdn.tailwindcss.com"></script>
         </head>
         <body class="bg-gray-100">{children}</body>
