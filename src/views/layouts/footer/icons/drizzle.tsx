be78a18d41012a41d7ec0e393ba2b7a91ff78f1e
zorin <andrei.zorin@gropyus.com>
2025-06-08 17:01:17 +0200

    refactor footer icons

--- src/views/layouts/footer/icons/drizzle.tsx
+++ src/views/layouts/footer/icons/drizzle.tsx
@@ -1,3 +1,4 @@
+import { css } from "hono/css";
 import { type IconVariantProp, sizes } from "./variants";
 
 export default function DrizzleIcon({ size = "sm" }: IconVariantProp) {
@@ -14,17 +15,16 @@ export default function DrizzleIcon({ size = "sm" }: IconVariantProp) {
 			<title>Drizzle Icon</title>
 			<path
 				d="M5.353 11.823a1.036 1.036 0 0 0 -0.395 -1.422 1.063 1.063 0 0 0 -1.437 0.399L0.138 16.702a1.035 1.035 0 0 0 0.395 1.422 1.063 1.063 0 0 0 1.437 -0.398l3.383 -5.903Zm11.216 0a1.036 1.036 0 0 0 -0.394 -1.422 1.064 1.064 0 0 0 -1.438 0.399l-3.382 5.902a1.036 1.036 0 0 0 0.394 1.422c0.506 0.283 1.15 0.104 1.438 -0.398l3.382 -5.903Zm7.293 -4.525a1.036 1.036 0 0 0 -0.395 -1.422 1.062 1.062 0 0 0 -1.437 0.399l-3.383 5.902a1.036 1.036 0 0 0 0.395 1.422 1.063 1.063 0 0 0 1.437 -0.399l3.383 -5.902Zm-11.219 0a1.035 1.035 0 0 0 -0.394 -1.422 1.064 1.064 0 0 0 -1.438 0.398l-3.382 5.903a1.036 1.036 0 0 0 0.394 1.422c0.506 0.282 1.15 0.104 1.438 -0.399l3.382 -5.902Z"
-				fill={styles.icon.color}
-				stroke-width="1"
-				opacity={styles.icon.opacity}
+				class={styles.path}
 			/>
 		</svg>
 	);
 }
 
 const styles = {
-	icon: {
-		color: "#ffffff",
-		opacity: 0.5,
-	},
+	path: css`
+		fill: var(--color-primary-text);
+		opacity: 0.5;
+        stroke-width: 0.25;
+    `,
 };
--- src/views/layouts/footer/icons/htmx.tsx
+++ src/views/layouts/footer/icons/htmx.tsx
@@ -1,3 +1,4 @@
+import { css } from "hono/css";
 import { type IconVariantProp, sizes } from "./variants";
 
 export default function HtmxIcon({ size = "sm" }: IconVariantProp) {
@@ -13,18 +14,17 @@ export default function HtmxIcon({ size = "sm" }: IconVariantProp) {
 		>
 			<title>Htmx Icon</title>
 			<path
+				class={styles.path}
 				d="M0 13.01v-2l7.09 -2.98 0.58 1.94 -5.1 2.05 5.16 2.05 -0.63 1.9Zm16.37 1.03 5.18 -2 -5.16 -2.09 0.65 -1.88L24 10.95v2.12L17 16zm-2.85 -9.98H16l-5.47 15.88H8.05Z"
-				fill={styles.icon.color}
-				stroke-width="1"
-				opacity={styles.icon.opacity}
 			/>
 		</svg>
 	);
 }
 
 const styles = {
-	icon: {
-		color: "#ffffff",
-		opacity: 0.5,
-	},
+	path: css`
+		fill: var(--color-primary-text);
+		opacity: 0.5;
+		stroke-width: 0.25;
+	`,
 };
